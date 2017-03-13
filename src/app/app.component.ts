eb4bc7903e34ec3bde9699b946008ff2e205ca3e
Anthony Guérot <anthony.guerot@mexedge.com>
2017-03-13 17:56:09 +0100

    integration auth0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
+import { AuthService } from './auth.service';
+
 import 'rxjs/Rx';
 
 @Component({
   selector: 'cortex-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ AuthService ]
 })
 export class AppComponent {
   isDarkTheme = false;
   offline: Observable<boolean>;
 
-  constructor() {
+  constructor(private auth: AuthService) {
     this.offline = Observable.merge(
       Observable.of(!navigator.onLine),
       Observable.fromEvent(window, 'online').map(() => false),
