e3833cbd8647d2cd09b902728e5000dded958d83
Anthony Guérot <anthony.guerot@mexedge.com>
2017-03-24 14:25:32 +0100

    #10 add missing app insight init upon merge...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
+import { AppInsightsService } from '@markpieszak/ng-application-insights';
 import { AuthService } from './auth.service';
 
 import 'rxjs/Rx';
@@ -15,11 +16,13 @@ export class AppComponent {
   isDarkTheme = false;
   offline: Observable<boolean>;
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private appInsightService: AppInsightsService) {
     this.offline = Observable.merge(
       Observable.of(!navigator.onLine),
       Observable.fromEvent(window, 'online').map(() => false),
       Observable.fromEvent(window, 'offline').map(() => true)
     );
+
+    appInsightService.init();
   }
 }
